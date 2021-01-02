75fcab8497e7636cdfd0b134a60aa8a2b1a1027d
Zen <ktvn100@gmail.com>
2021-01-02 16:34:28 +0700

    Create List Fragment

---
+++ app/src/main/java/com/hcmus/trello/ui/list/ListViewModel.java
@@ -0,0 +1,18 @@
+package com.hcmus.trello.ui.list;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ListViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public ListViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is list fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
